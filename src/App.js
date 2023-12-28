277187c97d7b0e2997f29c0c526702b9a97fecdd
Sumeet-SBJD <samkumarzi60@gmail.com>
2023-12-28 16:42:14 +0530

    todo app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-// import TodoList from './components/TodoList/TodoList';
-// import TodoForm from './components/TodoForm/TodoForm';
-import { Container } from 'react-bootstrap';
-import WeatherStackAPI from './Weather';
+import TodoList from './components/TodoList/TodoList';
+import TodoForm from './components/TodoForm/TodoForm';
+import { Container, Row, Col } from 'react-bootstrap';
+// import WeatherStackAPI from './Weather';
 
 function App() {
   return (
     <Container>
-        <WeatherStackAPI />
-         {/* <Row>
+        {/* <WeatherStackAPI /> */}
+         <Row>
             <Col md={3}>
               <TodoForm />
             </Col>
             <Col>
               <TodoList />
             </Col>
-         </Row> */}
+         </Row>
       </Container>
   );
 }
